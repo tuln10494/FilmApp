d860fd17fb526a2976765f265e70008befb97c34
lntu10494@gmail.com <lntu10494@gmail.com>
2025-01-12 09:24:15 +0700

    fix import

--- MyApplication/app/src/main/java/com/example/myapplication/ui/screens/home/HomeScreen.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/screens/home/HomeScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
